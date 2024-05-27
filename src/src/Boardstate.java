f0cacf4379c4ba1f4bf0bf90de4e0b233f68028e
thederpcodeman <connermckeone42@gmail.com>
2024-05-27 15:53:34 -0700

    Board size

--- src/src/Boardstate.java
+++ src/src/Boardstate.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 public class Boardstate {
     private ArrayList<ArrayList<Tile>> State;
     private ArrayList<Rule> Rules;
+
+    private int X;
+    private int Y;
     public Boardstate(int x, int y){
+        X = x;
+        Y = y;
         State = new ArrayList<>();
         for (int i = 0; i < x; i++){
             ArrayList<Tile> iAr = new ArrayList<>();
@@ -14,6 +19,6 @@ public class Boardstate {
             State.add(iAr);
         }
         Rules = new ArrayList<>();
-    } //creates a blank 8x8 and fresh rules.
+    } //creates a blank X*Y and fresh rules.
 
 }
