3105a5ffc11dbb8b38750957d90a0f57852b6943
thederpcodeman <connermckeone42@gmail.com>
2024-05-27 15:31:12 -0700

    Piece update

--- src/src/Piece.java
+++ src/src/Piece.java
@@ -1,10 +1,13 @@
 public abstract class Piece {
-    public Piece(){
+    Boolean Royal;
+    Boolean Protection;
+    int Value;
+    public Piece(Boolean R, Boolean P, int V){
+        Royal = R;
+        Protection = P;
+        Value = V;
+    }
+    abstract void update();
+    abstract int isLegalMove(int sX, int sY, int eX, int eY, Boardstate board); //0 = no, 1 = move, 2 =
 
-    }
-    public void update(){
-    }
-    public int isLegalMove(int sX, int sY, int eX, int eY, Boardstate board){ //0 = no, 1 = move, 2 =
-        return 0;
-    }
 }
---
+++ src/src/Rule.java
@@ -0,0 +1,3 @@
+public abstract class Rule {
+    public String name;
+}
