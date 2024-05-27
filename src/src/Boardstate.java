5238e7eee51a834288a14e841d67676526e36713
thederpcodeman <connermckeone42@gmail.com>
2024-05-27 15:50:56 -0700

    Board Init

--- src/src/Boardstate.java
+++ src/src/Boardstate.java
@@ -1,5 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Boardstate {
-    private ArrayList<ArrayList<Boardstate>> State = new ArrayList<>();
+    private ArrayList<ArrayList<Tile>> State;
+    private ArrayList<Rule> Rules;
+    public Boardstate(int x, int y){
+        State = new ArrayList<>();
+        for (int i = 0; i < x; i++){
+            ArrayList<Tile> iAr = new ArrayList<>();
+            for (int i2 = 0; i2 < y; i2++){
+                iAr.add(new Tile());
+            }
+            State.add(iAr);
+        }
+        Rules = new ArrayList<>();
+    } //creates a blank 8x8 and fresh rules.
+
 }
