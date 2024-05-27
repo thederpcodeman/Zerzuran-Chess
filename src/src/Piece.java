29ab16ea22c257ce88d70ed8b45b2cef7c980b9f
thederpcodeman <connermckeone42@gmail.com>
2024-05-27 15:22:55 -0700

    Added Piece superclass

--- src/src/Piece.java
+++ src/src/Piece.java
@@ -1,2 +1,10 @@
-public class Piece {
+public abstract class Piece {
+    public Piece(){
+
+    }
+    public void update(){
+    }
+    public int isLegalMove(int sX, int sY, int eX, int eY, Boardstate board){ //0 = no, 1 = move, 2 =
+        return 0;
+    }
 }
