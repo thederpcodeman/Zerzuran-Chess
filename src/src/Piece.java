cfbdbde1e49303e36c5136a13036fdbc43915038
thederpcodeman <connermckeone42@gmail.com>
2024-05-27 17:11:05 -0700

    Ladies and gentlemen, whe have HONSE w/ff check

--- src/src/Piece.java
+++ src/src/Piece.java
@@ -1,12 +1,14 @@
 public abstract class Piece {
-    Boolean Royal;
-    Boolean Protection;
-    int Value;
-    public Piece(Boolean R, Boolean P, int V){
+    public boolean Royal;
+    public boolean Protection;
+    public int Value;
+    public int Color;
+    public Piece(Boolean R, Boolean P, int V, int C){
         Royal = R;
         Protection = P;
         Value = V;
+        Color = C;
     }
     abstract void update();
-    abstract int isLegalMove(Move m); //0 = No, 1 = Move, 2 = Destroy, 3 = Swap, 4 = En pessant
+    abstract int isLegalMove(Move m); //0 = No, 1 = Move, 2 = Destroy, 3 = Swap, 4 = En pessant, 5 = ffMove, 6 = ffDestroy
 }
