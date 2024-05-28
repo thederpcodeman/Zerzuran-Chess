9c0ff0e87b7deeda7a035157f2ea25541161c4a3
connertm24 <connermckeone42@gmail.com>
2024-05-28 09:07:30 -0700

    work on score

--- src/src/ScoreMove.java
+++ src/src/ScoreMove.java
@@ -1,5 +1,9 @@
 public class ScoreMove {
     public static double score(Boardstate c, int d){
+        if (d <= 0 || !stillGoing(c)){
+            return fScore(c);
+        }
+        return 0;//waiting on getlegalmoves
 
     }
 
@@ -23,5 +27,26 @@ public class ScoreMove {
         }
         return ((wV * Math.sqrt(wR)) - (bV * Math.sqrt(bR)));
 
+    }
+
+    public static boolean stillGoing(Boardstate b){
+        int wP = 0;
+        int bP = 0;
+        boolean wR = false;
+        boolean bR = false;
+        for (Piece i : b.getPieces()){
+            if (i.Color == 1){
+                wP++;
+                if (i.Royal){
+                    wR = true;
+                }
+            }else if (i.Color == 2) {
+                bP++;
+                if (i.Royal) {
+                    bR = true;
+                }
+            }
+        }
+        return (wR && bR && (wP >= 2) && (bP >= 2));
     }
 }
