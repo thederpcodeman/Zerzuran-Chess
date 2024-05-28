6277807b296e316481fb6a6a808d62f74fa5de82
thederpcodeman <connermckeone42@gmail.com>
2024-05-27 17:37:44 -0700

    added check to prevent non-moving moves

--- src/src/Tile.java
+++ src/src/Tile.java
@@ -13,6 +13,9 @@ public class Tile {
     }
 
     public boolean isLegalMove(Move m){
+        if ((m.sX == m.eX) && (m.sY == m.eY)){
+            return false;
+        }
         int i = piece.isLegalMove(m);
         if (i == 0 || i == 5 || i == 6) {
             return false;
