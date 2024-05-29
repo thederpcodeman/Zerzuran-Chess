24a92e57de3cebb5fd5eb9422d7fbe6ef07b291c
connertm24 <connermckeone42@gmail.com>
2024-05-29 09:06:13 -0700

    I think it's making every possible move at once, except two white kings are here for some reason???

--- src/src/Main.java
+++ src/src/Main.java
@@ -5,29 +5,29 @@ public class Main {
         Boardstate b = new Boardstate(8, 8);
 
         b.setTile(0,0, new Rook(0));
-        b.setTile(1,0, new Knight(0));
-        b.setTile(2,0, new Bishop(0));
-        b.setTile(3,0, new King(0));
-        b.setTile(4,0, new Queen(0));
-        b.setTile(5,0, new Bishop(0));
-        b.setTile(6,0, new Knight(0));
-        b.setTile(7,0, new Rook(0));
+        b.setTile(0,1, new Knight(0));
+        b.setTile(0,2, new Bishop(0));
+        b.setTile(0,3, new King(0));
+        b.setTile(0,4, new Queen(0));
+        b.setTile(0,5, new Bishop(0));
+        b.setTile(0,6, new Knight(0));
+        b.setTile(0,7, new Rook(0));
 
         for (int i = 0; i < 8; i++){
-            b.setTile(i,1, new Pawn(0));
+            b.setTile(1,i, new Pawn(0));
         }
 
-        b.setTile(0,7, new Rook(1));
-        b.setTile(1,7, new Knight(1));
-        b.setTile(2,7, new Bishop(1));
-        b.setTile(3,7, new King(1));
-        b.setTile(4,7, new Queen(1));
-        b.setTile(5,7, new Bishop(1));
-        b.setTile(6,7, new Knight(1));
+        b.setTile(7,0, new Rook(1));
+        b.setTile(7,1, new Knight(1));
+        b.setTile(7,2, new Bishop(1));
+        b.setTile(7,3, new King(1));
+        b.setTile(7,4, new Queen(1));
+        b.setTile(7,5, new Bishop(1));
+        b.setTile(7,6, new Knight(1));
         b.setTile(7,7, new Rook(1));
 
         for (int i = 0; i < 8; i++){
-            b.setTile(i,6, new Pawn(1));
+            b.setTile(6,i, new Pawn(1));
         }
 
         while (true){
